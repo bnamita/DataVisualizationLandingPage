910d940a2bc9c7344c1c092443c8e64075a477a0
nbhatawdekar <autocad7>
2016-02-27 16:32:20 +0530

    updated urls

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
-render([{"id":"1","name":"IndianBudgetZoomable", title: "Indian Budget - Revenue sources"},
-    {"id":"2","name":"IndianExpenditureZoomable", title: "Indian Budget - Expenditure"},
-    {"id":"3","name":"IndianExpenditureByMinistry", title: "Expenditure By Ministry"},
-    {"id":"4","name":"IndianExpenditureByMinistryComparison", title: "Indian Budget by Ministry - YoY comparison"}]);
+render([{"id":"1","name":"IndianBudgetZoomable", "url":"2016/02/blog-post.html" ,title: "Where does the money come from?"},
+    {"id":"2","name":"IndianExpenditureZoomable", "url": "2016/02/where-does-money-go.html", title: "Where does the money go?"},
+    {"id":"3","name":"IndianExpenditureByMinistry", "url":"2016/02/expenditure-by-ministry.html"  ,title: "Expenditure By Ministry"},
+    {"id":"4","name":"IndianExpenditureByMinistryComparison", "url": "2016/02/indias-expenditure-by-ministry.html", title: "Expenditure by Ministry - YoY comparison"}]);
 
 
 function render(viz_list) {
@@ -13,7 +13,8 @@ function render(viz_list) {
         .data(viz_list, function(d) { return d.id; })
         .enter().insert("a", "br")
         .attr("class", "viz viz--thumbnail")
-        .attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
+        //.attr("href", function(d) { return "//bnamita.github.io/" + d.name }) //blog links
+        .attr("href", function(d) { return "//thedatajournal.blogspot.in/" + d.url }) //blog links
         .style("background-image", function(d) { return "url(css/images/" + d.name + ".png)"; });
 
 //        gist.append("div")
